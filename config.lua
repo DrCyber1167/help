b9f8319bb489b1246d63cd4854c8b3f7332b4277
IDeactive <IDeactive@users.noreply.github.com>
2016-04-18 17:03:50 +0430

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,22 +1,12 @@
 return {
-
-	-- Your authorization token from the botfather.
-	bot_api_key = '',
-	-- Your Telegram ID.
-	admin = '',
-	-- Differences, in seconds, between your time and UTC.
-	time_offset = 0,
-	-- Two-letter language code.
-	lang = 'en',
-	-- The channel, group, or user to send error reports to.
-	-- If this is not set, errors will be printed to the console.
-	log_chat = nil,
-	-- http://console.developers.google.com
-	google_api_key = '',
-	-- https://cse.google.com/cse
-	google_cse_key = '',
-	-- http://thecatapi.com/docs.html
-	thecatapi_key = '',
+	bot_api_key = '', -- Your authorization token from the botfather.
+	admins = {174747020,140529465,YUORID}, -- Your Telegram ID.
+	time_offset = 0, -- Differences, in seconds, between your time and UTC.
+	lang = 'en', -- Two-letter language code.
+	log_chat = nil, -- The channel, group, or user to send error reports to. --If this is not set, errors will be printed to the console.
+	google_api_key = '', 	-- http://console.developers.google.com
+	google_cse_key = '', -- https://cse.google.com/cse
+	thecatapi_key = '', -- http://thecatapi.com/docs.html
 	
 	errors = { -- Generic error messages used in various plugins.
 		connection = 'Connection error.',
