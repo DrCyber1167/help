69cd7266e4862aa58a0d83ab6d07499a96f6551e
parsa13881 <mohammadpa87@gmail.com>
2016-04-20 11:40:30 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 return {
-	bot_api_key = '', -- Your authorization token from the botfather.
-	admins = {174747020,140529465,YUORID}, -- Your Telegram ID.
+	bot_api_key = '201783465:AAHyFzKn3OWWPheogm6T1iSKMgaTJBmMmOc', -- Your authorization token from the botfather.
+	admins = {105831687}, -- Your Telegram ID.
 	time_offset = 0, -- Differences, in seconds, between your time and UTC.
 	lang = 'en', -- Two-letter language code.
 	log_chat = nil, -- The channel, group, or user to send error reports to. --If this is not set, errors will be printed to the console.
